7ed6b9fcefd492b8f8ffebc58e925682ac5f370d
ctrembath <claretrembath@ymail.com>
2015-01-05 17:01:09 +0000

    added sorting drop down, can sort alphabetically and by newest

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -32,5 +32,30 @@
       query.sendKeys('nexus');
       expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
     });
+
+    it('should be possible to control phone order via the drop down select menu', function (){
+      var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
+      var query = element(by.model('query'));
+
+      function getNames() {
+        return phoneNameColumn.map(function(elm) {
+          return elm.getText();
+        });
+      }
+
+        query.sendKeys('tablet');
+
+        expect(getNames()).toContain([
+          "Motorola XOOM\u2122 with Wi-Fi, 'The Next, Next Generation Experience the future with MOTOROLA XOOM, the world's first tablet powered by Android 3.0 (Honeycomb).",
+          "MOTOROLA XOOM\u2122"
+          ]);
+
+        element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+
+        expect(getNames()).toContain([
+          "MOTOROLA XOOM\u2122, 'The Next, Next Generation Experience the future with MOTOROLA XOOM, the world's first tablet powered by Android 3.0 (Honeycomb).",
+          "Motorola XOOM\u2122 with Wi-Fi"
+          ]);
+    });
   });
 });
