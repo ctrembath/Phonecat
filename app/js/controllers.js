acee1a24f949dfeeee1fcce794650b7df4dce6a7
ctrembath <claretrembath@ymail.com>
2015-01-05 17:34:43 +0000

    only displays 5 phones

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,7 @@ phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
   function($scope, $http) {
   $http.get('phones/phones.json').success(function(data) {
     $scope.phones = data
+    $scope.phones = data.splice(0, 5);
   });
 
  $scope.orderProp = 'age';
